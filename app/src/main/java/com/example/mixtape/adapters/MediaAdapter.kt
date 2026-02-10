b31db1d9c1c7984c5911cd64a9b8ca77b47d49e5
Daniel-Varshavsky <dnvaop99@gmail.com>
2026-02-11 00:15:43 +0200

    Update song item icon in `MediaAdapter`
    - Change the icon for `MediaItem.SongItem` from `outline_music_note_2_24` to `sharp_music_note_2_24`.

--- app/src/main/java/com/example/mixtape/adapters/MediaAdapter.kt
+++ app/src/main/java/com/example/mixtape/adapters/MediaAdapter.kt
@@ -35,7 +35,7 @@ class MediaAdapter(
 
         // Set icon based on media type
         when (item) {
-            is MediaItem.SongItem -> holder.icon.setImageResource(R.drawable.outline_music_note_2_24)
+            is MediaItem.SongItem -> holder.icon.setImageResource(R.drawable.sharp_music_note_2_24)
             is MediaItem.VideoItem -> holder.icon.setImageResource(R.drawable.outline_videocam_24)
         }
 
